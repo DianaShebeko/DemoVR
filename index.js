cb7691e2e3af2f8a05c36326fb4db3fbb1f33363
Diana <dianasebeko155@gmail.com>
2026-05-12 21:29:35 +0300

    HIDE SCENElIST

--- index.js
+++ index.js
@@ -145,10 +145,10 @@
   sceneListToggleElement.addEventListener('click', toggleSceneList);
 
   // Start with the scene list open on desktop.
-  if (!document.body.classList.contains('mobile')) {
+  /*if (!document.body.classList.contains('mobile')) {
     showSceneList();
   }
-
+ */
   // Set handler for scene switch.
   scenes.forEach(function(scene) {
       var el = document.querySelector('#sceneList .scene[data-id="' + scene.data.id + '"]');
